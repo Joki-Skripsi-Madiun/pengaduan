d92edf21cdff448d22ee82f2d66558972aa4bdb9
chesiyanoviawulandari <wulandarichesiyanovia@gmail.com>
2023-06-27 10:37:49 +0700

    layout surat

--- app/Views/laporan/print_pelanggaran_siswa.php
+++ app/Views/laporan/print_pelanggaran_siswa.php
@@ -1,75 +1,206 @@
-<?= $this->extend('layout/template'); ?>
-<?= $this->section('content'); ?>
-<!--  Header End -->
-<div class="container-fluid">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12">
-                <div class="card w-100">
-                    <div class="card-body p-4">
-                        <h5 class="card-title fw-semibold mb-4">Data Prestasi</h5>
-                        <right><a class="d-none d-sm-inline-block btn btn-sm btn-success shadow-sm" href="<?= base_url(); ?>/">
-                                Cetak</a></right>
-                        <br>
-                        <br>
-                        <br>
-                        <div class="table-responsive">
-                            <table id="data1" class="table table-avatar bg-grey text-black">
-                                <thead class="text-dark fs-4">
-                                    <tr>
-                                        <th class="border-bottom-0">
-                                            <h6 class="fw-semibold mb-0">Id</h6>
-                                        </th>
-                                        <th class="border-bottom-0">
-                                            <h6 class="fw-semibold mb-0">Nama Siswa</h6>
-                                        </th>
-                                        <th class="border-bottom-0">
-                                            <h6 class="fw-semibold mb-0">Jenis Prestasi</h6>
-                                        </th>
-                                        <th class="border-bottom-0">
-                                            <h6 class="fw-semibold mb-0">Aksi</h6>
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php foreach ($joinprestasi as $u) : ?>
-                                        <tr>
-                                            <td class="border-bottom-0">
-                                                <h6 class="fw-semibold mb-0">1</h6>
-                                            </td>
-                                            <td class="border-bottom-0">
-                                                <h6 class="fw-semibold mb-1"><?= $u['nama_siswa'] ?></h6>
+<!doctype html>
+<html lang="en">
 
-                                            </td>
-                                            <td class="border-bottom-0">
-                                                <h6 class="fw-semibold mb-1"><?= $u['nama_prestasi'] ?></h6>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.84.0">
+    <title>Pelanggaran & Prestasi</title>
+    <link rel="shortcut icon" href="<?= base_url(); ?>/img/logosekolah.png" />
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/dashboard/">
 
-                                            </td>
-                                            <td class="border-bottom-0">
-                                                <div class="d-flex align-items-center gap-2">
-                                                    <a class="badge bg-warning rounded-2 fw-semibold" href="<?= base_url(); ?>/prestasi/edit/<?= $u['id_prestasi'] ?>">
-                                                        Edit</a>
-                                                    <a class="badge bg-success rounded-2 fw-semibold" href="<?= base_url(); ?>/prestasi/view/<?= $u['id_prestasi'] ?>">
-                                                        Detail</a>
-                                                    <form action="<?= base_url(); ?>/prestasi/<?= $u['id_prestasi']; ?>" method="post" class="d-inline">
-                                                        <?= csrf_field(); ?>
-                                                        <input type="hidden" name="_method" value="DELETE">
-                                                        <button type="submit" class="d-none d-sm-inline-block btn btn-sm btn-danger shadow-sm" onclick="return confirm('Apakah Anda Yakin ?');"><i class="fas fa-edit fa-sm text-white-50"></i> Hapus</a> </button>
+    <!-- icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
-                                                    </form>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    <?php endforeach ?>
+    <!-- Template Stylesheet -->
+    <link href="<?= base_url(); ?>/assets/css/style.css" rel="stylesheet">
 
-                                </tbody>
-                            </table>
-                        </div>
+
+    <style>
+        /* Red border */
+        hr.new1 {
+            border-top: 5px black;
+        }
+
+        /* Dashed red border */
+        hr.new2 {
+            border-top: 1px dashed red;
+        }
+
+        /* Dotted red border */
+        hr.new3 {
+            border-top: 1px dotted red;
+        }
+
+        /* Thick red border */
+        hr.new4 {
+            border: 1px solid black;
+        }
+
+        /* Large rounded green border */
+        hr.new5 {
+            border: 10px solid green;
+            border-radius: 5px;
+        }
+
+        p.a {
+            font-family: "Times New Roman", Times, serif;
+        }
+
+        table {
+            border-collapse: collapse;
+        }
+
+        table.stats {
+            border-collapse: separate;
+        }
+    </style>
+</head>
+
+<body>
+
+    <table width='100%'>
+        <tr>
+            <td>
+                <img src="<?= base_url(); ?>/img/logo_kab.png" height="100px">
+
+            </td>
+            <td>
+                <center>
+                    <div style="font-size: 15px; font-family:'Times New Roman', Times, serif;"><b>PEMERINTAH KABUPATEN MADIUN<br>
+                            DINAS PENDIDIKAN DAN KEBUDAYAAN
+                        </b></div>
+                    <div style="font-size: 20px; font-family:'Times New Roman', Times, serif;"><b>SEKOLAH MENENGAH PERTAMA NEGERI 2 NGLAMES</b></div>
+                    <div style="font-size: 15px; font-family:'Times New Roman', Times, serif;">Desa Sendangrejo Telp.(0351) 499049 Kecamatan Madiun
+                        <br>Website : www.smpn2nglames.sch.id Email : smpn2_nglames@yahoo.co.id<br>MADIUN 63151
                     </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 
-<?= $this->endSection(); ?>
+                </center>
+            </td>
+        </tr>
+    </table>
+    <hr class="new4">
+    <p align="right" style="font-size: 15px; font-family:'Times New Roman', Times, serif;">Madiun, </p>
+    <table width='100%'>
+
+        <tr>
+
+            <td style="font-size: 15px; font-family:'Times New Roman', Times, serif;">Nomor</td>
+            <td>:</td>
+            <td style="font-size: 15px; font-family:'Times New Roman', Times, serif;">360/ /402.302/2023 &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; </td>
+            <td> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</td>
+            <td style="font-size: 15px; font-family:'Times New Roman', Times, serif;">&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Kepada</td>
+
+
+        </tr>
+        <tr>
+
+            <td style="font-size: 15px; font-family:'Times New Roman', Times, serif;">NIP/Sifat</td>
+            <td>:</td>
+            <td style="font-size: 15px; font-family:'Times New Roman', Times, serif;">Segera</td>
+            <td> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</td>
+            <td style="font-size: 15px; font-family:'Times New Roman', Times, serif;">&nbsp; &nbsp; &nbsp; &nbsp;Yth. Orang Tua Siswa Kelas VI A </td>
+
+        </tr>
+        <tr>
+
+            <td style="font-size: 15px; font-family:'Times New Roman', Times, serif;">Hal</td>
+            <td>:</td>
+            <td style="font-size: 15px; font-family:'Times New Roman', Times, serif;">Undangan</td>
+            <td> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</td>
+            <td style="font-size: 15px; font-family:'Times New Roman', Times, serif;">&nbsp; &nbsp; &nbsp; &nbsp;di-
+                TEMPAT</td>
+
+        </tr>
+        <!-- <tr>
+
+            <td style="font-size: 15px; font-family:'Times New Roman', Times, serif;">Hal</td>
+            <td>:</td>
+            <td style="font-size: 15px; font-family:'Times New Roman', Times, serif;"></td>
+            <td> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</td>
+            <td style="font-size: 15px; font-family:'Times New Roman', Times, serif;">&nbsp; &nbsp; &nbsp; &nbsp;
+            </td>
+
+
+        </tr> -->
+
+    </table>
+    <br>
+    <div style="font-size: 15px; font-family:'Times New Roman', Times, serif;"> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Mengharap dengan hormat kehadiran Bapak / Ibu / Saudara orang tua / Wali Murid besok pada :</div>
+
+    <table width='80%' align="center">
+
+        <tr>
+
+            <td style="font-size: 15px; font-family:'Times New Roman', Times, serif;"> Hari, Tanggal </td>
+            <td>:</td>
+            <td style="font-size: 15px; font-family:'Times New Roman', Times, serif;">Tanah Longsor </td>
+
+        </tr>
+        <tr>
+
+            <td style="font-size: 15px; font-family:'Times New Roman', Times, serif;">Waktu</td>
+            <td>:</td>
+            <td style="font-size: 15px; font-family:'Times New Roman', Times, serif;">Dsn. Golang Ds. Kuwiran RT 06 RW 02 Kec. Kare</td>
+
+        </tr>
+        <tr>
+
+            <td style="font-size: 15px; font-family:'Times New Roman', Times, serif;">Acara</td>
+            <td>:</td>
+            <td style="font-size: 15px; font-family:'Times New Roman', Times, serif;"></td>
+
+        </tr>
+        <tr>
+
+            <td style="font-size: 15px; font-family:'Times New Roman', Times, serif;">Tempat</td>
+            <td>:</td>
+            <td style="font-size: 15px; font-family:'Times New Roman', Times, serif;"></td>
+
+        </tr>
+
+
+    </table>
+    <br>
+    <div style="font-size: 15px; font-family:'Times New Roman', Times, serif;">Demikian surat undangan ini di buat untuk dipenuhi, atas perhatian dan kerjasamanya di sampaikan terima kasih</div>
+    <br><br>
+    <table width='100%'>
+        <tr>
+            <td>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</td>
+            <td style="font-size: 15px; font-family:'Times New Roman', Times, serif;">
+                <center>Kepala SMP Negeri 2 Nglames</center>
+            </td>
+        </tr>
+        <tr>
+            <td>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</td>
+            <td style="font-size: 15px; font-family:'Times New Roman', Times, serif;">
+                <center><br>
+                    <br>
+                    <br><br><br><br>
+                </center>
+            </td>
+        </tr>
+        <tr>
+            <td>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</td>
+            <td style="font-size: 15px; font-family:'Times New Roman', Times, serif;">
+                <center><b><u> WIDODO, S.Pd</u></b><br>Pembina Tingkat I<br>NIP. 196902201997031005
+                </center>
+            </td>
+        </tr>
+
+    </table>
+
+
+
+    <script>
+        window.print();
+    </script>
+
+
+
+</body>
+
+
+</html>
